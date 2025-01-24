8e388c648fa2b7886493d13fbd08908153ed68a1
Keith Thibodeaux <keith@railscoder.net>
2025-01-24 18:05:02 -0500

    use add_die and remove_die

--- farkle.ino
+++ farkle.ino
@@ -3,17 +3,49 @@
 
 PCF8575 score_display(0x21);
 int score = 0;
+int dice[6] = { 0, 0, 0, 0, 0, 0 };
+
+void add_die(int number) {
+  for (int i = 0; i < 6; i++) {
+    if (dice[i] == 0) {
+      dice[i] = number;
+      break;
+    }
+  }
+}
+
+void remove_die() {
+  for (int i = 5; i >= 0; i--) {
+    if (dice[i] != 0) {
+      dice[i] = 0;
+      break;
+    }
+  }
+}
+
+void clear() {
+  for (int i = 0; i < 6; i++) {
+    dice[i] = 0;
+  }
+}
+
+void bank() {
+  score = score + calculate_score(dice);
+}
 
 void setup() {
   Wire.begin();
 
   display_score_setup();
-  bank();
-}
 
-void bank() {
-  int dice[6] = {4, 4, 4, 4, 4, 5};
-  score = score + calculate_score(dice);
+  add_die(6);
+  add_die(6);
+  add_die(6);
+  add_die(6);
+  remove_die();
+  add_die(5);
+
+  bank();
 }
 
 void loop() {
